272ac1f1c942515a2d9266d22a11f6dee8865be3
Nicolas Roussel <n.roussel13@gmail.com>
2020-12-12 19:08:01 +0100

    Setup input parser

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,7 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 pub fn run() {
+    parse_input("./inputs/day1/example").unwrap();
     println!("Day 1 solution");
 }
 
-fn parseInput(file: &str) -> Vec<i32> {
-    return (0..10).collect();
+fn parse_input(filename: &str) -> io::Result<i32> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+
+    for line in (io::BufReader::new(&file)).lines() {
+        if let Ok(value) = line {
+            println!("{}", value)
+        }
+    }
+
+    Ok(1)
 }
