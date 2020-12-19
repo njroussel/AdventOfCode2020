a730c92da80ea161321c46c5b8f5b196f545b0eb
Nicolas Roussel <n.roussel13@gmail.com>
2020-12-19 18:10:02 +0100

    Finish day 5

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -1,12 +1,12 @@
+use std::cmp;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
 pub fn run() {
     println!("Day 5 solution.");
 
     println!("Parsing example input...");
-    let input = parse_input("./inputs/day4/example").unwrap();
+    let input = parse_input("./inputs/day5/example").unwrap();
     println!("Parsed.");
 
     println!("Solving...");
@@ -14,7 +14,7 @@ pub fn run() {
     println!("Solution: {}", count);
 
     println!("Parsing input...");
-    let input = parse_input("./inputs/day4/input").unwrap();
+    let input = parse_input("./inputs/day5/input").unwrap();
     println!("Parsed.");
 
     println!("Solving...");
@@ -24,27 +24,76 @@ pub fn run() {
 
 fn solution(passports: &Vec<BoardingPass>, detail: bool) -> i32 {
     if detail {
-        println!("lol");
+        for passport in passports {
+            println!("{:?}", passport)
+        }
     }
-    0
+
+    passports
+        .iter()
+        .fold(0, |max, passport| cmp::max(max, passport.seat_id))
 }
 
 #[derive(Debug)]
 struct BoardingPass {
-    row: Option<i32>,
-    column: Option<i32>,
-    seat_id: Option<i32>,
+    row: i32,
+    column: i32,
+    seat_id: i32,
 }
 
 impl BoardingPass {
     pub fn from_str(input_str: &String) -> io::Result<BoardingPass> {
         let mut out = BoardingPass {
-            row: None,
-            column: None,
-            seat_id: None,
+            row: 0,
+            column: 0,
+            seat_id: 0,
         };
 
+        let row_chars: Vec<char> = input_str.get(..7).unwrap().chars().collect();
+        let column_chars: Vec<char> = input_str.get(7..).unwrap().chars().collect();
+
+        let row_partioning_idx: Vec<bool> = row_chars.iter().map(|x| *x == 'F').collect();
+        let row_idx = bsp(&row_partioning_idx, 0, 0, 127);
+
+        let column_partioning_idx: Vec<bool> = column_chars.iter().map(|x| *x == 'L').collect();
+        let column_idx = bsp(&column_partioning_idx, 0, 0, 7);
+
+        out.row = row_idx;
+        out.column = column_idx;
+        out.seat_id = out.row * 8 + out.column;
+
         Ok(out)
+    }
+}
+
+fn bsp(partioning_idx: &Vec<bool>, partition_idx: usize, start_idx: i32, end_idx: i32) -> i32 {
+    let len = end_idx - start_idx + 1;
+
+    if len == 2 {
+        match partioning_idx[partition_idx] {
+            true => return start_idx,
+            false => return end_idx,
+        }
+    }
+
+    let half_len = len / 2;
+    match partioning_idx[partition_idx] {
+        true => {
+            return bsp(
+                partioning_idx,
+                partition_idx + 1,
+                start_idx,
+                end_idx - half_len,
+            )
+        }
+        false => {
+            return bsp(
+                partioning_idx,
+                partition_idx + 1,
+                start_idx + half_len,
+                end_idx,
+            )
+        }
     }
 }
 
@@ -53,25 +102,12 @@ fn parse_input(filename: &str) -> io::Result<Vec<BoardingPass>> {
     let file = File::open(&path)?;
 
     let mut passports: Vec<BoardingPass> = Vec::new();
-    let mut lines: Vec<String> = Vec::new();
-
-    fn concat_lines(lines: &Vec<String>) -> String {
-        lines
-            .iter()
-            .fold("".to_string(), |acc, line| format!("{} {}", acc, line))
-    }
 
     for line in (io::BufReader::new(&file)).lines() {
         if let Ok(value) = line {
-            lines.push(value.trim().to_string());
-            if (&lines[lines.len() - 1]).eq("") {
-                passports.push(BoardingPass::from_str(&concat_lines(&lines))?);
-                lines.clear();
-            }
+            passports.push(BoardingPass::from_str(&value)?);
         }
     }
-
-    passports.push(BoardingPass::from_str(&concat_lines(&lines))?);
 
     Ok(passports)
 }
